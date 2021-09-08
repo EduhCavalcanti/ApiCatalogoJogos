dbba2900e98b46294e2b6b44d7c6b723459479a8
EduhCavalcanti <eduh.cavalcanti@hotmail.com>
2021-09-08 18:16:32 -0300

    Terminando de implementar o repositorio de jogo

--- ApiCatalogoJogos/Repositorie/JogoRepositorio.cs
+++ ApiCatalogoJogos/Repositorie/JogoRepositorio.cs
@@ -9,31 +9,45 @@ namespace ApiCatalogoJogos.Repositorie
 {
     public class JogoRepositorio : IJogoRepositorio
     {
-        
+        //Simular o banco de dados
+        private static Dictionary<Guid, Jogos> jogos = new Dictionary<Guid, Jogos>(){
 
-        Task<List<Jogos>> IJogoRepositorio.Obter(int pagina, int quantidade)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task<Jogos> IJogoRepositorio.ObterPorId(Guid id)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task<List<Jogos>> IJogoRepositorio.Obter(string nome, string produtora)
+            {Guid.Parse("0ca314a5-9282-45d8-92c3-2985f2a9fd04"), new Jogos{ Id = Guid.Parse("0ca314a5-9282-45d8-92c3-2985f2a9fd04"), Nome = "Fifa 21", Produtora = "EA", Preco = 200} },
+            {Guid.Parse("eb909ced-1862-4789-8641-1bba36c23db3"), new Jogos{ Id = Guid.Parse("eb909ced-1862-4789-8641-1bba36c23db3"), Nome = "Fifa 20", Produtora = "EA", Preco = 190} },
+            {Guid.Parse("5e99c84a-108b-4dfa-ab7e-d8c55957a7ec"), new Jogos{ Id = Guid.Parse("5e99c84a-108b-4dfa-ab7e-d8c55957a7ec"), Nome = "Fifa 19", Produtora = "EA", Preco = 180} },
+            {Guid.Parse("da033439-f352-4539-879f-515759312d53"), new Jogos{ Id = Guid.Parse("da033439-f352-4539-879f-515759312d53"), Nome = "Fifa 18", Produtora = "EA", Preco = 170} },
+            {Guid.Parse("92576bd2-388e-4f5d-96c1-8bfda6c5a268"), new Jogos{ Id = Guid.Parse("92576bd2-388e-4f5d-96c1-8bfda6c5a268"), Nome = "Street Fighter V", Produtora = "Capcom", Preco = 80} },
+            {Guid.Parse("c3c9b5da-6a45-4de1-b28b-491cbf83b589"), new Jogos{ Id = Guid.Parse("c3c9b5da-6a45-4de1-b28b-491cbf83b589"), Nome = "Grand Theft Auto V", Produtora = "Rockstar", Preco = 190} }
+        };
+        public Task Atualizar(Jogos obj)
         {
             throw new NotImplementedException();
         }
 
         public Task Inserir(Jogos obj)
         {
-            throw new NotImplementedException();
+            jogos.Add(obj.Id, obj);
+            return Task.CompletedTask;
+        }
+        
+        //Para fazer paginação
+        public Task<List<Jogos>> Obter(int pagina, int quantidade)
+        {
+            return Task.FromResult(jogos.Values.Skip((pagina - 1) * quantidade).Take(quantidade).ToList());
         }
 
-        public Task Atualizar(Jogos obj)
+        public Task<List<Jogos>> Obter(string nome, string produtora)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(jogos.Values.Where(jogo => jogo.Nome.Equals(nome) && jogo.Produtora.Equals(produtora)).ToList());
+        }
+
+        public Task<Jogos> ObterPorId(Guid id)
+        {
+            //Verifica se existe no Dicionario, se não existir retorna null
+            if(!jogos.ContainsKey(id)){
+                return Task.FromResult<Jogos>(null);
+            }
+            return Task.FromResult(jogos[id]);
         }
 
         public Task Remover(Guid id)
